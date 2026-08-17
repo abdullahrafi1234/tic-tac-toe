65f202bd3e7d8259196af4a5c48186619d8baa5a
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-08-17 12:43:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Square from "./components/Square";
+
+function App() {
+  return (
+    <>
+      <div className="flex">
+        {" "}
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        {" "}
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        {" "}
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <>
+      <button
+        onClick={handleClick}
+        className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      >
+        {value}
+      </button>
+    </>
+  );
+}
