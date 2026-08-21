ee2e7b9ca7c6057ad71c58949d5524d1f13cf752
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-08-21 19:15:52 +0600

    game dashboard done

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,8 @@
-export default function Square({ value }) {
-  // const [value, setValue] = useState(null);
-
-  function handleClick() {}
+export default function Square({ value, onSquareClick }) {
   return (
     <>
       <button
-        onClick={handleClick}
+        onClick={onSquareClick}
         className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
       >
         {value}
